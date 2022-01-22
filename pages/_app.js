242abc191f49697367124243eef2f5bf1c107042
James <jamail090999@gmail.com>
2022-01-22 15:47:38 +0000

    added user context and debug page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,40 @@
+import { useState, createContext } from "react";
+
+import Head from "next/head";
 import Container from "../components/Container/Container";
+import Nav from "../components/nav/Nav";
 import "../styles/globals.css";
 
+const userContext = createContext();
+
 const App = ({ Component, pageProps }) => {
+  const [user, setUser] = useState({
+    kakapo_id: "",
+    display_name: "",
+    token: "",
+    isAuthenticated: false,
+  });
+
   return (
-    <Container>
-      <Component {...pageProps} />
-    </Container>
+    <>
+      <Head>
+        <title>Kakapo</title>
+        <meta name="description" content="Kakapo Social Platform" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"
+          rel="stylesheet"
+        />
+      </Head>
+      <userContext.Provider value={{ user, setUser }}>
+        <Nav />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </userContext.Provider>
+    </>
   );
 };
 
+export { userContext };
 export default App;
---
+++ pages/debug.js
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+
+import { userContext } from "./_app";
+
+import styles from "../styles/debug.module.css";
+
+const notSetText = "Not set";
+
+const Debug = () => {
+  const { user } = useContext(userContext);
+
+  return (
+    <div className={styles.debug}>
+      <h2>Current User</h2>
+      <div className={styles.user}>
+        <ul>
+          <li>
+            <h4>Kakapo ID</h4>
+            <p>{user.kakapo_id || notSetText}</p>
+          </li>
+          <li>
+            <h4>Display Name</h4>
+            <p>{user.display_name || notSetText}</p>
+          </li>
+          <li>
+            <h4>Is Authenticated</h4>
+            <p>{user.isAuthenticated ? "Yes" : "No"}</p>
+          </li>
+          <li>
+            <h4>Token</h4>
+            <p>{user.token || notSetText}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Debug;
