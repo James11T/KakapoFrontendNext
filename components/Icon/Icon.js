ecb229c27f212e9d26b9e8ae503808ff5d04ef87
James <jamail090999@gmail.com>
2022-01-22 13:13:26 +0000

    added ligature bypass

--- components/Icon/Icon.js
+++ components/Icon/Icon.js
@@ -1,11 +1,36 @@
+import classNames from "classnames";
 import styles from "./Icon.module.css";
 
-const Icon = ({ src }) => {
+const ligatureCodes = {
+  favorite: "&#xe87d;",
+  like: "&#xe87d;",
+  question_answer: "&#xe8af;",
+  comment: "&#xe8af;",
+  flag: "&#xe153;",
+  report: "&#xe153;",
+  bookmark: "&#xe866;",
+  save: "&#xe866;",
+  home: "&#xe88a;",
+  search: "&#xe8b6;",
+  explore: "&#xe8b6;",
+  inbox: "&#xe156;",
+  person: "&#xe7fd;",
+};
+
+/*
+  Attempt to change ligature to raw icon
+*/
+
+const Icon = ({ children }) => {
+  const icon = children.startsWith("&#x")
+    ? children
+    : ligatureCodes[children] || children;
+
   return (
-    <img
-      src={src.endsWith("svg" ? src : `${src}.svg`)}
-      className={styles.icon}
-    />
+    <span
+      className={classNames("icon", styles.icon)}
+      dangerouslySetInnerHTML={{ __html: icon }}
+    ></span>
   );
 };
 
